11608b028530573fb428153bfd40d33e9a4cad19
Jacqueline Lim <jacweizhen6679@gmail.com>
2024-03-16 20:45:47 +0800

    Update script.js

--- Playable-Piano/Playable Piano/script.js
+++ Playable-Piano/Playable Piano/script.js
@@ -4,9 +4,9 @@ window.onload = () => {
   //24keys
   for (let index = 1; index <= 24; index++) {
     let div = document.createElement("div");
-    div.classList.add("key", index <= 10 ? "black--key" : "key");
+    div.classList.add("key", index <= 11 ? "black--key" : "key");
     //For playing audio on click
-    const number = index <= 9 ? "0" + index : index;
+    const number = index <= 10 ? "0" + index : index;
     div.addEventListener("click", () => {
       new Audio(`${base}key${number}.mp3`).play();
     });
